8f8445b1e5eb25fd77f7944a5e47c0f4eb1238b0
Js459 <Js459@LAPTOP-IGV2V9UJ>
2023-03-19 07:12:49 +0100

    Projektdateien hinzufügen.

---
+++ Taschenrechner_Iteration_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner_Iteration_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
+            Console.Write("Bitte gib den ersten Summanden ein: ");
+            string ersterSummand = Console.ReadLine();
+            Console.Write("Bitte gib den zweiten Summanden ein: ");
+            string zweiterSummand = Console.ReadLine();
+
+            // Wandel Text in Ganzzahlen
+            int ersterSummandAlsZahl = Convert.ToInt32(ersterSummand);
+            int zweiterSummandAlsZahl = Convert.ToInt32(zweiterSummand);
+
+            // Berechnung ausführen
+            int summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+
+            // Ausgabe 
+            Console.WriteLine("Die Summe ist: {0}", summe);
+            Console.ReadLine();
+        }
+    }
+}
