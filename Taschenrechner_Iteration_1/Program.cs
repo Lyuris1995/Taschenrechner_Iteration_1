ef541e407d88e0b9fe2c4d92e30e5d2ddac957ea
Julian Schmidt <Js4595@hotmail.com>
2023-04-02 18:00:10 +0200

    * und / funktion hinzugefügt.

--- Taschenrechner_Iteration_1/Program.cs
+++ Taschenrechner_Iteration_1/Program.cs
@@ -23,46 +23,78 @@ namespace Taschenrechner_Iteration_1
             // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
             string ersteZahlAlsString = HoleBenutzerEingabe("Bitte gib die erste Zahl ein: ");
             string zweiteZahlAlsString = HoleBenutzerEingabe("Bitte gib die zweite Zahl ein ein: ");
-            string operation = HoleBenutzerEingabe("Bitte gib die auszuführende Operation an (+ oder -): ");
+            string operation = HoleBenutzerEingabe("Bitte gib die auszuführende Operation an (+, -, * oder /): ");
 
             // Wandel Text in Gleitkommazahlen
             // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist.
             double ersteZahl = Convert.ToDouble(ersteZahlAlsString);
             double zweiteZahl = Convert.ToDouble(zweiteZahlAlsString);
 
-            // Berechnung ausführen
-            double resultat = 0;
-            switch (operation)
-            {
-                case "+":
-                    resultat = Addiere(ersteZahl, zweiteZahl);
-                    Console.WriteLine("Die Summe ist: {0}", resultat);
-                    break;
-
-                case "-":
-                    resultat = Subtrahiere(ersteZahl, zweiteZahl);
-                    Console.WriteLine("Die Differenz ist: {0}", resultat);
-                    break;
-
-                case "*":   
-                case "/":
-                    Console.WriteLine("Diese Operation wird bald unterstützt. Versprochen! :)");
-                    break;
-
-                default:
-                    Console.WriteLine("Du hast eine ungültige Auswahl der Operation getroffen.");
-                    break;
-            }           
+            //Berechnung ausführen
+            double resultat = Berechnung(ersteZahl, zweiteZahl, operation);
 
             // Ausgabe 
+            GibResultatAus(resultat, operation);
             HoleBenutzerEingabe("Zum beenden bitte Return drücken!");
         }
+
         static string HoleBenutzerEingabe(string ausgabeText)
         {
             Console.Write(ausgabeText);
             string Summand = Console.ReadLine();
 
             return Summand;
+        }
+
+        static double Berechnung(double ersteZahl, double zweiteZahl, string operation)
+        {
+            // Berechnung ausführen
+            double resultat = 0;
+            switch (operation)
+            {
+                case "+":
+                    resultat = Addiere(ersteZahl, zweiteZahl);
+                    break;
+
+                case "-":
+                    resultat = Subtrahiere(ersteZahl, zweiteZahl);
+                    break;
+
+                case "*":
+                    resultat = Multipliziere(ersteZahl, zweiteZahl);
+                    break;
+
+                case "/":
+                    resultat = Dividieren(ersteZahl, zweiteZahl);
+                    break;
+            }
+            return resultat;
+        }
+
+        static void GibResultatAus(double resultat, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine("Die Summe ist: {0}", resultat);
+                    break;
+
+                case "-":
+                    Console.WriteLine("Die Differenz ist: {0}", resultat);
+                    break;
+
+                case "*":
+                    Console.WriteLine("Der Wert des Quotienten: {0}", resultat);
+                    break;
+
+                case "/":
+                    Console.WriteLine("Das Produkt ist: {0}", resultat);
+                    break;
+
+                default:
+                    Console.WriteLine("Du hast eine ungültige Auswahl der Operation getroffen.");
+                    break;
+            }
         }
 
         static double Addiere(double ersterSummand, double zweiterSummand)
@@ -76,6 +108,14 @@ namespace Taschenrechner_Iteration_1
             double summe = minuend - subtrahent;
 
             return summe;
+        }
+        static double Multipliziere(double multiplikator, double multiplikand)
+        {
+            return multiplikator * multiplikand;
+        }
+        static double Dividieren(double dividend, double divisor)
+        {
+            return dividend / divisor;
         }
 
     }
