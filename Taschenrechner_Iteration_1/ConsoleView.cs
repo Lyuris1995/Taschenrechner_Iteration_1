78d5eae2510a8034b7e4cb776049a21584ce1883
Julian Schmidt <Js4595@hotmail.com>
2023-04-10 11:22:23 +0200

    Klasse ConsoleView angelegt und Methoden aus Program.cs in die klasse reinverlagert.

---
+++ Taschenrechner_Iteration_1/ConsoleView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner_Iteration_1
+{
+    internal class ConsoleView
+    {
+        public string HoleBenutzerEingabe(string ausgabeText)
+        {
+            Console.Write(ausgabeText);
+            string Summand = Console.ReadLine();
+
+            return Summand;
+        }
+
+        public void GibResultatAus(double resultat, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine("Die Summe ist: {0}", resultat);
+                    break;
+
+                case "-":
+                    Console.WriteLine("Die Differenz ist: {0}", resultat);
+                    break;
+
+                case "*":
+                    Console.WriteLine("Der Wert des Quotienten: {0}", resultat);
+                    break;
+
+                case "/":
+                    Console.WriteLine("Das Produkt ist: {0}", resultat);
+                    break;
+
+                default:
+                    Console.WriteLine("Du hast eine ungültige Auswahl der Operation getroffen.");
+                    break;
+            }
+        }
+    }
+}
--- Taschenrechner_Iteration_1/Program.cs
+++ Taschenrechner_Iteration_1/Program.cs
@@ -21,10 +21,10 @@ namespace Taschenrechner
 
         static void Main(string[] args)
         {
-            // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
-            string ersteZahlAlsString = HoleBenutzerEingabe("Bitte gib die erste Zahl ein: ");
-            string zweiteZahlAlsString = HoleBenutzerEingabe("Bitte gib die zweite Zahl ein ein: ");
-            string operation = HoleBenutzerEingabe("Bitte gib die auszuführende Operation an (+, -, * oder /): ");
+            ConsoleView view = new ConsoleView();
+            string ersteZahlAlsString = view.HoleBenutzerEingabe("Bitte gib die erste Zahl ein: ");
+            string zweiteZahlAlsString = view.HoleBenutzerEingabe("Bitte gib die zweite Zahl ein ein: ");
+            string operation = view.HoleBenutzerEingabe("Bitte gib die auszuführende Operation an (+, -, * oder /): ");
 
             // Wandel Text in Gleitkommazahlen
             // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist.
@@ -36,42 +36,10 @@ namespace Taschenrechner
             model.Berechne(ersteZahl, zweiteZahl, operation);
 
             // Ausgabe 
-            GibResultatAus(model.Resultat, operation);
-            HoleBenutzerEingabe("Zum beenden bitte Return drücken!");
+            view.GibResultatAus(model.Resultat, operation);
+            view.HoleBenutzerEingabe("Zum beenden bitte Return drücken!");
         }
 
-        static string HoleBenutzerEingabe(string ausgabeText)
-        {
-            Console.Write(ausgabeText);
-            string Summand = Console.ReadLine();
-
-            return Summand;
-        }
-
-        static void GibResultatAus(double resultat, string operation)
-        {
-            switch (operation)
-            {
-                case "+":
-                    Console.WriteLine("Die Summe ist: {0}", resultat);
-                    break;
-
-                case "-":
-                    Console.WriteLine("Die Differenz ist: {0}", resultat);
-                    break;
-
-                case "*":
-                    Console.WriteLine("Der Wert des Quotienten: {0}", resultat);
-                    break;
-
-                case "/":
-                    Console.WriteLine("Das Produkt ist: {0}", resultat);
-                    break;
-
-                default:
-                    Console.WriteLine("Du hast eine ungültige Auswahl der Operation getroffen.");
-                    break;
-            }
-        }
+        
     }
 }
